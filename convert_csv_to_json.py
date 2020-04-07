c3f7f4b3b3b9752be1eca0c2d7f7fc113d0b362b
sitrucp <sitrucp@users.noreply.github.com>
2020-04-07 13:10:17 -0400

    clean up file names, cases source from github cases.csv instead of google sheet file

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -1,10 +1,7 @@
 import csv, json
 
-#csvfile = open('Public_COVID-19_Canada.csv', 'r',encoding='utf-8') 
-#jsonfile = open('Public_COVID-19_Canada.json', 'w',encoding='utf-8')
-
 csvfile = 'Public_COVID-19_Canada_final.csv'
-jsonfile = 'Public_COVID-19_Canada.json'
+jsonfile = 'Public_COVID-19_Canada_final.json'
 
 with open(csvfile) as f:
     reader = csv.DictReader(f)
